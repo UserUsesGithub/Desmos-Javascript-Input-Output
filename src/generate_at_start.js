e9f4b853cc29d9b88669afc78e85e2909476ae9a
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2025-06-16 22:03:20 -0400

    Create generate_at_start.js

---
+++ src/generate_at_start.js
@@ -0,0 +1,23 @@
+// Note that, unless this has been set up when making the graph, it will erase it
+const expressions=Calc.getExpressions()
+for (let i = 0; i < 7; i++) {
+  console.log(expressions[i])
+  if ((expressions[i] !== undefined)) {
+      if ((expressions[i].id)===("IoLibParameters"+i.toString())) {
+          isAlreadyGenerated = true
+      }
+  }
+}
+
+console.log(isAlreadyGenerated)
+if (!isAlreadyGenerated) {
+    Calc.setBlank()
+    for (let i = 0; i < 7; i++) {
+      Calc.setExpression({
+        latex: String.raw`V_{${i}}=0`,
+        id: watchID + i.toString(),
+        secret: true
+      });
+    }
+}
+Calc.removeExpression({id:'1'})
