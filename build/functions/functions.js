1e19fbe9768e73851b8392e32f66aa98b69d4a23
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2025-06-16 22:12:55 -0400

    Create functions.js

---
+++ build/functions/functions.js
@@ -0,0 +1,7 @@
+let functions=[
+    ((v1,v2,v3,v4,v5) => {return 0}),
+    ((v1,v2,v3,v4,v5) => {return Math.sqrt(Number(v1))}),
+    ((v1,v2,v3,v4,v5) => {return Number(v1)*Number(v1)}),
+    ((v1,v2,v3,v4,v5) => {return ListToDesmos([v1,v1,v1])}),
+    
+]
