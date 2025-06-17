dd71a1a96e481ac548d8fc29453fc8c4ccacd190
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2025-06-16 22:20:02 -0400

    Create list_of_functions_and_values.js

---
+++ build/list_of_functions_and_values.js
@@ -0,0 +1,9 @@
+//These are the functions and the watch ID. The functions are in a list.
+const watchID = "IoLibParameters";
+let functions=[
+    ((v1,v2,v3,v4,v5) => {return 0}),
+    ((v1,v2,v3,v4,v5) => {return Math.sqrt(Number(v1))}),
+    ((v1,v2,v3,v4,v5) => {return Number(v1)*Number(v1)}),
+    ((v1,v2,v3,v4,v5) => {return ListToDesmos([v1,v1,v1])}),
+    
+]
